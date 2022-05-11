980fa590e20e904e8621f1c2c7e4e140bd508f4a
aiden mcmanus <89177158+grecosenko@users.noreply.github.com>
2022-05-10 23:57:31 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 var iframe = document.createElement("iframe");
-iframe.setAttribute("src", "https://grecosenko.github.io/hijij/");
+iframe.setAttribute("src", "https://grecosenko.github.io/hijij/home.html");
 iframe.setAttribute("style", "position:fixed; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;");
 document.body.appendChild(iframe);
 
