a95cbb1e113508f99160d6cb7d35ff5483414b0a
aiden mcmanus <89177158+grecosenko@users.noreply.github.com>
2022-10-10 09:58:02 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,14 @@
 
-var iframe = document.createElement("iframe");
-iframe.setAttribute("src", "https://soggy.monacanhumanities.net");
-iframe.setAttribute("style", "position:fixed; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;");
-document.body.appendChild(iframe);
-
-
-var smile = " welcome to the best place";
-
-var fileName = "welcome.txt";
-
-var fileContents = smile;
-
-var a = document.createElement('a');
-
-a.href = 'data:attachment/text,' + encodeURI(fileContents);
-
-a.target = '_blank';
-
-a.download = fileName;
-
-document.body.appendChild(a);
-
-a.click();
-
-window.frames["iframe"].contentDocument.oncontextmenu = function(){
- return false; 
-};
-
+//Creates new ABC
+var page = new ABC({
+    "type": "blob", //Blank, blob, or overwrite
+    "url": "https://soggy.monacanhumanities.net" //Any url
+})
+//Set the type
+page.setType("blob")
+//Set the url
+page.setUrl("https://soggy.monacanhumanities.net")
+//Get iframe code
+console.log(page.getCode())
+//Open page
+page.open()
