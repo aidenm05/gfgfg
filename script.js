ce57eb01a480aa74ea2e70c31c1f2490fcc71462
aiden mcmanus <89177158+grecosenko@users.noreply.github.com>
2022-10-25 16:44:26 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,12 @@
 //Creates new ABC
 var page = new ABC({
     "type": "blob", //Blank, blob, or overwrite
-    "url": "https://soggy.monacanhumanities.net" //Any url
+    "url": "https://chromebook.ccpsadmin.net" //Any url
 })
 //Set the type
 page.setType("blob")
 //Set the url
-page.setUrl("https://soggy.monacanhumanities.net")
+page.setUrl("https://chromebook.ccpsadmin.net")
 //Get iframe code
 console.log(page.getCode())
 //Open page
