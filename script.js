8ffc7d6c94ca2051f87f30425c299ec1228b28e5
aiden mcmanus <89177158+grecosenko@users.noreply.github.com>
2022-11-01 14:44:27 -0400

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-
-var page = window.open()
-page.document.body.innerHTML = `<iframe style="height:100%; width: 100%; border: none; position: fixed; top: 0; right: 0; left: 0; bottom: 0; border: none" sandbox="allow-modals allow-orientation-lock allow-pointer-lock allow-presentation allow-same-origin allow-scripts allow-top-navigation allow-top-navigation-by-user-activation" src="` + 'chromebook.cccpsadmin.net' + `"></iframe>`
-} catch {
-
-}
-}
