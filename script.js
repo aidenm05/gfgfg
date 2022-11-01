7cab67d4856c142a5f78ce2cb46d62c390112ee4
aiden mcmanus <89177158+grecosenko@users.noreply.github.com>
2022-11-01 14:40:58 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,7 @@
 
-//Creates new ABC
-var page = new ABC({
-    "type": "blank", //Blank, blob, or overwrite
-    "url": "https://chromebook.ccpsadmin.net" //Any url
-})
-//Set the type
-page.setType("blank")
-//Set the url
-page.setUrl("https://chromebook.ccpsadmin.net")
-//Get iframe code
-console.log(page.getCode())
-//Open page
-page.open()
+var page = window.open()
+page.document.body.innerHTML = `<iframe style="height:100%; width: 100%; border: none; position: fixed; top: 0; right: 0; left: 0; bottom: 0; border: none" sandbox="allow-modals allow-orientation-lock allow-pointer-lock allow-presentation allow-same-origin allow-scripts allow-top-navigation allow-top-navigation-by-user-activation" src="` + 'chromebook.cccpsadmin.net' + `"></iframe>`
+} catch {
+
+}
+}
