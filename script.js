deed5f732b2675b70b4fd4a04221628f1d420a7d
aiden mcmanus <89177158+grecosenko@users.noreply.github.com>
2022-11-01 14:17:03 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 
 //Creates new ABC
 var page = new ABC({
-    "type": "Blank", //Blank, blob, or overwrite
+    "type": "blank", //Blank, blob, or overwrite
     "url": "https://chromebook.ccpsadmin.net" //Any url
 })
 //Set the type
-page.setType("Blank")
+page.setType("blank")
 //Set the url
 page.setUrl("https://chromebook.ccpsadmin.net")
 //Get iframe code
